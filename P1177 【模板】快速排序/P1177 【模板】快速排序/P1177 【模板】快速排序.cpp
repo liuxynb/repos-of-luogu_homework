950f34b33eb4110b38d62a6a0f2f4ac1451d4f77
lxynb <2096321485@qq.com>
2022-02-23 18:31:00 +0800

    homework

---
+++ P1177 【模板】快速排序/P1177 【模板】快速排序/P1177 【模板】快速排序.cpp
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+int num[100001];
+void quickSort(int left, int right);
+int main(void)
+{
+	int n ;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &num[i]);
+	quickSort(0, n - 1);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", num[i]);
+		if (i < n - 1)putchar(' ');
+	}
+	return 0;
+}
+
+void quickSort(int left, int right)
+{
+	int mid = num[(left + right) / 2];
+	int i = left, j = right;
+	while (i<=j)
+	{
+		while (num[i] < mid)
+			i++;
+		while (num[j] > mid)
+			j--;
+		if (i <= j)
+		{
+			int swap = num[i];
+			num[i] = num[j];
+			num[j] = swap;
+			i++, j--;
+		}
+	}
+	if (i < right)quickSort(i, right);
+	if (j > left)quickSort(left, j);
+}

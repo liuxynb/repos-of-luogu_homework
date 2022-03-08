454e8d78eb1153268f6895c622aa7b07fdbdb579
liuxynb <2096321485@qq.com>
2022-03-08 22:46:07 +0800

    3.8

---
+++ P1024/P1024/P1024.cpp
@@ -0,0 +1,46 @@
+﻿#include<stdio.h>
+double a, b, c, d;
+double f(double x);
+int main(void)
+{
+
+	double left, right, mid;
+	int temp = 0;
+	scanf("%lf%lf%lf%lf", &a, &b, &c, &d);
+	for (int i = -100; i <= 100; i++)
+	{
+		left = i, right = i + 1;
+		if (f(left) == 0)
+		{
+			printf("%.2f", left); 
+			temp++;
+			if (temp <= 2) putchar(' ');
+			else break;
+			continue;
+		}
+		if (f(left) * f(right) < 0)
+		{
+			for (int j = 0; j < 30; j++)
+			{
+				mid = (left + right) / 2;
+				if (f(left) * f(mid) < 0)
+					right = mid;
+				else
+					left = mid;
+			}
+			printf("%.2f", left);
+			temp++;
+			if (temp <= 2) putchar(' ');
+			else break;
+		}
+
+	}
+	return 0;
+}
+
+
+double f(double x)
+{
+	return a * x * x * x + b * x * x + c * x + d;
+}
+	
---
+++ tiaoshi/tiaoshi/tiaoshi.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	char str[10000];
+	int num;
+	while (scanf("%d", &num) == 1)
+		;
+	scanf("%s", str);
+	printf("%s", str);
+	return 0;
+}
